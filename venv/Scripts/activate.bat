ec858e99e47b5e3da267d70a45723bb91b11676d
Wesley Antonio <57929638+WesleyDev184@users.noreply.github.com>
2022-11-17 00:50:29 -0300

    Criação de uma nova venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\waarq\Documents\Projetos\DRF_API\venv
+set VIRTUAL_ENV=C:\Users\waarq\Documents\Projetos\django_rest_api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
